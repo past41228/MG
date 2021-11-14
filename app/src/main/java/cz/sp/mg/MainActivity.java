939990001c8053267554e833aaa6d74004752a3f
past41228 <past41228@gmail.com>
2021-11-14 17:01:30 +0100

    Fixed block control during pause.

--- app/src/main/java/cz/sp/mg/MainActivity.java
+++ app/src/main/java/cz/sp/mg/MainActivity.java
@@ -2,13 +2,16 @@ package cz.sp.mg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     private BlocksView blocks;
 
+    @SuppressLint("ClickableViewAccessibility") // TODO https://developer.android.com/guide/topics/ui/accessibility/custom-views#custom-touch-events
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,16 +19,23 @@ public class MainActivity extends AppCompatActivity {
         this.blocks = findViewById(R.id.blocks);
 
 
-        findViewById(R.id.leftButton).setOnClickListener(v -> blocks.moveLeft()); // TODO touch
-        findViewById(R.id.rightButton).setOnClickListener(v -> blocks.moveRight()); // TODO touch
-        findViewById(R.id.upButton).setOnClickListener(v -> {
+        findViewById(R.id.leftButton).setOnClickListener((View v) -> {
+            if (blocks.isRunning()) blocks.moveLeft();
+        });
+        findViewById(R.id.rightButton).setOnClickListener((View v) -> {
+            if (blocks.isRunning()) blocks.moveRight();
+        });
+        findViewById(R.id.upButton).setOnClickListener((View v) -> {
             if (blocks.isRunning()) {
                 blocks.pause();
             } else {
                 blocks.resume();
             }
         });
-        findViewById(R.id.downButton).setOnTouchListener((v, event) -> {
+        findViewById(R.id.downButton).setOnTouchListener((View v, MotionEvent event) -> {
+            if (!blocks.isRunning()) {
+                return false;
+            }
             switch (event.getAction()) {
                 case MotionEvent.ACTION_DOWN:
                     blocks.speedUp();
